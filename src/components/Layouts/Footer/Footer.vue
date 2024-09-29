92e55a2b9b6f57f27ef3390b54f711545195516b
theoxav <theoxxav@outlook.fr>
2024-09-29 23:22:02 +0200

    Add Header and Footer components

--- src/components/Layouts/Footer/Footer.vue
+++ src/components/Layouts/Footer/Footer.vue
@@ -1,9 +1,11 @@
 <template>
-  <footer>
-    <h1>Footer</h1>
+  <footer class="d-flex flex-row justify-content-center align-items-center">
+    <p>Copyright © 2014-2024 TheoXav</p>
   </footer>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import './Footer.scss';
+</style>
--- src/components/Layouts/Header/Header.vue
+++ src/components/Layouts/Header/Header.vue
@@ -1,9 +1,30 @@
 <template>
-  <header>
-    <h1>Header</h1>
+  <header class="px-20 d-flex flex-row align-items-center">
+    <a href="#" class="d-flex flex-row align-items-center mr-20">
+      <img src="@/assets/logo.svg" alt="logo-app" />
+      <span class="logo">Shopy</span>
+    </a>
+    <ul class="d-flex flex-row flex-fill">
+      <li class="mr-10">
+        <a href="#">Shop</a>
+      </li>
+      <li>
+        <a href="#">Admin</a>
+      </li>
+    </ul>
+    <ul class="d-flex flex-row">
+      <li class="mr-10">
+        <a href="#">Inscription</a>
+      </li>
+      <li>
+        <a href="#">Connexion</a>
+      </li>
+    </ul>
   </header>
 </template>
 
 <script setup lang="ts"></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import './Header.scss';
+</style>
